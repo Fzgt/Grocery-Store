f5f17cad1bbb8cebf6c189361eacb6dd2a78868a
Fzgt <fzgt320@gmail.com>
2025-03-20 22:35:02 +1100

    init: vite react+swc template

--- Prometheus/src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const planeSize = 1000;
-export const planeTextureSize = planeSize / 20;
-
-export const shipSpeed = 0.9;
-
-export const cubeSize = 20;
-export const cubeCount = 400;
--- Prometheus/src/utils.ts
+++
@@ -1,3 +0,0 @@
-export const randomInRange = (from: number, to: number) => {
-    return Math.floor(Math.random() * (to - from + 1)) - to;
-}
